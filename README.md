91567c7b490134a5d15891f60fc668baf1549b7a
Jacky Su <jacky19918@gmail.com>
2021-10-15 13:34:58 +0800

    升級至3.1.20，加入範例，更新README.md

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # CipherLib
 A quick way to encrypt specific fields in appsettings.json for .NET Core
+
+In fact, this refers to the practice on the Internet and makes some modifications to make it easier to use on .NET Core, and has not done a complete security assessment. Sometimes security and convenience are mutually exclusive, please choose the best a way that suits your convenience and part of the security.
+
+Which article to refer to has also been commented in the code.
+
+ ## Getting Started
+
+Assuming your appsettings.json is as follows
+```json
+{
+    "SomeApi": {
+        "EndPoint": "https://fakeapi.com/resource",
+        "Secret": "yourSecret"
+    },
+    "SomeOtherSetting": "Some text that doesn't need to be encrypted",
+    "DBConnection": "Server=YourSQLServer;Database=YourDB;Persist Security Info=True;User ID=YourUser;Password=YourPassword;"
+}
+```
+And you originally used AddJsonFile
+```C#
+new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+```
+Now, use CipherLib
+```C#
+using CipherLib.Extensions;
+```
+Since a password is used for symmetric encryption, please use a safe way to obtain your password.
+```C#
+var password = GetPasswordFromEnvironmentVariable();
+//or
+var password = GetPasswordFromFile();
+//or
+var password = GetPasswordFromSomewhere();
+```
+Use AddProtectedJsonFile instead of AddJsonFile, and please add regular expressions to indicate which fields need to be encrypted.
+```C#
+new ConfigurationBuilder()
+.AddProtectedJsonFile(
+    password,
+    "appsettings.json", 
+    optional: false,
+    reloadOnChange: true, 
+    new Regex("SomeApi:Secret"), 
+    new Regex("DBConnection"))
+```
+Build your program. If the fields in your appsettings.json have not been encrypted, the fields in the build folder will be encrypted. If the fields have been encrypted, they will not be encrypted again.
+
+You can view the example in the TestExample project
---
+++ TestExample/Program.cs
@@ -0,0 +1,77 @@
+﻿using CipherLib.Extensions;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Text.RegularExpressions;
+
+namespace TestExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var password = GetPasswordFromEnvironmentVariable();
+            //var password = GetPasswordFromFile();
+            var password = "yourPassword";
+
+            var configuration = new ConfigurationBuilder()
+                .AddProtectedJsonFile(password, "appsettings.json", 
+                    optional: false,
+                    reloadOnChange: true,
+                    new Regex("SomeApi:Secret"),
+                    new Regex("DBConnection"))
+                .Build();
+
+            Console.WriteLine(JsonConvert.SerializeObject(Serialize(configuration), Formatting.Indented));
+            Console.ReadKey();
+        }
+        private static JToken Serialize(IConfiguration config)
+        {
+            JObject obj = new JObject();
+
+            foreach (var child in config.GetChildren())
+            {
+                if (child.Path.EndsWith(":0"))
+                {
+                    var arr = new JArray();
+
+                    foreach (var arrayChild in config.GetChildren())
+                    {
+                        arr.Add(Serialize(arrayChild));
+                    }
+
+                    return arr;
+                }
+                else
+                {
+                    obj.Add(child.Key, Serialize(child));
+                }
+            }
+
+            if (!obj.HasValues && config is IConfigurationSection section)
+            {
+                if (bool.TryParse(section.Value, out bool boolean))
+                {
+                    return new JValue(boolean);
+                }
+                else if (decimal.TryParse(section.Value, out decimal real))
+                {
+                    return new JValue(real);
+                }
+                else if (int.TryParse(section.Value, out int integer))
+                {
+                    return new JValue(integer);
+                }
+                else if (long.TryParse(section.Value, out long longInteger))
+                {
+                    return new JValue(longInteger);
+                }
+
+                return new JValue(section.Value);
+            }
+
+            return obj;
+        }
+    }
+}
